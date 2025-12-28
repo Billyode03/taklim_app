1b5b90a471565d336d4d2cf074720117ec65da28
Naubill Ode <naubilode@gmail.com>
2025-12-28 10:57:40 +0800

    Update font

---
+++ lib/utils/constants/textConstant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:taklim_app/utils/constants/color_constant.dart';
+
+class TextContants {
+  static const TextStyle textLogo =
+      TextStyle(fontFamily: 'Roboto', color: ColorConstant.greenYoung);
+}
--- lib/views/login/login_screen.dart
+++ lib/views/login/login_screen.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:taklim_app/utils/constants/color_constant.dart';
+import 'package:taklim_app/utils/constants/image_constant.dart';
+import 'package:taklim_app/utils/constants/textConstant.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 30,
+              Padding(
+                padding: EdgeInsets.only(top: 56, bottom: 40),
+                child: Center(
+                  child: Image.asset(
+                    width: 170,
+                    height: 100,
+                    ImageConstant.logoUtama,
+                  ),
+                ),
               ),
-              Center(
-                child: Text(
-                  'LOGIN USER',
-                  style: TextStyle(
-                    fontSize: 20,
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Username',
+                style: TextContants.textLogo,
+              ),
+              Container(
+                width: 350,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: ColorConstant.white,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: ColorConstant.greenYoung,
                   ),
                 ),
               ),
@@ -34,43 +49,60 @@ class LoginScreen extends StatelessWidget {
                 width: 350,
                 height: 50,
                 decoration: BoxDecoration(
-                  color: Colors.cyan,
+                  color: ColorConstant.white,
                   borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: ColorConstant.greenYoung,
+                  ),
                 ),
+              ),
+              SizedBox(
+                height: 20,
               ),
               SizedBox(
                 height: 20,
               ),
               Container(
                 width: 350,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.cyan,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 150,
-                height: 50,
+                height: 45,
                 child: Center(
                   child: Text(
                     'Masuk',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: ColorConstant.white,
                       fontSize: 17,
                     ),
                   ),
                 ),
                 decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(8),
+                  color: ColorConstant.greenYoung,
+                  borderRadius: BorderRadius.circular(10),
                 ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: 350,
+                height: 45,
+                child: Center(
+                  child: Row(
+                    children: [
+                      Text(
+                        'Masuk Dengan Google',
+                        style: TextStyle(
+                          color: ColorConstant.greenYoung,
+                          fontSize: 17,
+                        ),
+                      ),
+                      // Image.asset(name)
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    color: ColorConstant.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: ColorConstant.greenYoung)),
               ),
             ],
           ),
