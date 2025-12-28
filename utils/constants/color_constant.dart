5d033dae457efaaacf674caca634cbc8e4055638
Naubill Ode <naubilode@gmail.com>
2025-12-28 10:09:02 +0800

    update constant

---
+++ utils/constants/color_constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color greenYoung = Color(0xFF07796b);
+
+}
---
+++ utils/constants/image_constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ImageConstant {
+  static const String logoUtama = 'assets/image/logo_utama.png'; 
+
+  
+}
